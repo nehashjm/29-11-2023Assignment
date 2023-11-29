43414934d708532a3c77c1677753eb80abb74725
Nehash <nehash.j.m027@gmail.com>
2023-11-29 15:37:13 +0530

    initialcommit

---
+++ Day18Assignment/Day18Assignment/Spin.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day18Assignment
+{
+    internal class Spin
+    {
+        //int num;
+        //internal double Choose(Double EnergyLevel,double Winbrobiablity)
+        //{
+            
+
+        //    if(num == 1)
+        //    {
+        //        return EnergyLevel + 10;
+        //        return Winbrobiablity + 10;
+        //    }
+
+        //}
+
+    }
+}
